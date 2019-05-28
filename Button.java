7c6a3ef3d29d956d11b300a7b4bf9aab5b99dd37
jackphilbrick <42852269+jackphilbrick@users.noreply.github.com>
2019-05-27 21:03:14 -0700

    Add files via upload
    Added new features like checking for neighboring bombs and displaying the bomb count on the grid

--- Button.java
+++ Button.java
@@ -1,14 +1,26 @@
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 public class Button extends JButton{
 
 	private boolean bombStatus;
 	private boolean isClicked;
+	private boolean clickListener;
 	
-	public Button() {
+	private int pointX;
+	private int pointY;
+	
+	public Button(int pointX, int pointY) {
 		super();
 		bombStatus = false;
 		isClicked = false;
+		clickListener = false;
+		
+		this.pointX = pointX;
+		this.pointY = pointY;
+		
 	}
 	
 	public void setBombStatus(boolean status) {
@@ -25,6 +37,32 @@ public class Button extends JButton{
 	
 	public boolean getIsClicked() {
 		return this.isClicked;
+	}
+	
+	public void setClickListener(boolean status) {
+		this.clickListener = true;
+	}
+	
+	public boolean getClickListener() {
+		return this.clickListener;
+	}
+	
+	public int getPointX() {
+		return this.pointX;
+	}
+	
+	public int getPointY() {
+		return this.pointY;
+	}
+
+	public void actionPerformed(int button1) {
+		System.out.println(this.pointX + " " + this.pointY);
+		
+	}
+
+	public void setBackground(Icon newIcon) {
+		
+		
 	}
 
 }
